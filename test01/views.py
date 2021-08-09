c8119d1835cd882182957df51aa8ae5e5b5af835
unknown <yu.suzuki.forjob@gmail.com>
2021-08-10 00:06:40 +0900

    配列を作成

--- test01/views.py
+++ test01/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from django.shortcuts import render
 
 def index(request):
-    text_sample = 'Love Live!'
-    context = {'text_sample': text_sample}
+    text_sample_lists = ['Love Live!',
+                        'スバラシイコエノヒト'
+    ]
+    context = {'text_sample_lists': text_sample_lists}
     return render(request, 'test01/index.html', context)
